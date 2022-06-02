c83ce03675fb1097d5fcff5b39f12f923f9cbb6e
Andrei Belokopytov <belokopytov87@gmail.com>
2022-06-02 17:52:40 +0000

    Добавлена кнопка подключения кошелька

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
+import { useAddress, useMetamask } from "@thirdweb-dev/react";
+
 const App = () => {
+  const address = useAddress();
+  const connectWithMetamask = useMetamask();
+
+  if (address) {
+    return (
+      <div className="landing">
+        <h1>👀 Готовы начать?</h1>
+      </div>
+    );
+  }
+
   return (
     <div className="landing">
-      <h1>Welcome to My DAO</h1>
+      <h1>Добро пожаловать в FogelDAO</h1>
+      <button className="btn-hero" onClick={connectWithMetamask}>
+        Подключить кошелек
+      </button>
     </div>
   );
 };
